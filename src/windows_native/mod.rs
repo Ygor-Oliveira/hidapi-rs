c87ab25a72f7ddd4fc046f59145158483749043b
Ygor Oliveira <ygor.oliveira@fluigent.com>
2024-02-23 10:48:42 +0100

    Change the other CancelIo call to CancelIoEx

--- src/windows_native/mod.rs
+++ src/windows_native/mod.rs
@@ -46,7 +46,7 @@ use windows_sys::Win32::Storage::FileSystem::{
     OPEN_EXISTING,
 };
 use windows_sys::Win32::System::Threading::ResetEvent;
-use windows_sys::Win32::System::IO::{CancelIo, CancelIoEx, DeviceIoControl};
+use windows_sys::Win32::System::IO::{CancelIoEx, DeviceIoControl};
 
 const STRING_BUF_LEN: usize = 128;
 
@@ -179,7 +179,7 @@ impl HidDeviceBackendBase for HidDevice {
             if res != TRUE {
                 let err = Win32Error::last();
                 if err != Win32Error::IoPending {
-                    unsafe { CancelIo(self.device_handle.as_raw()) };
+                    unsafe { CancelIoEx(self.device_handle.as_raw(), state.overlapped.as_raw()) };
                     self.read_pending.set(false);
                     return Err(err.into());
                 }
